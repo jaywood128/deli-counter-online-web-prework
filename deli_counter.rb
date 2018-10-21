faffe32e6d88a22e3e99b224d8c33f3b00c82119
Johnathon Wood <jaywood128@gmail.com>
2018-10-21 18:44:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
-if line(array) 
-  if array.length == 0 
+if line(array)
+  if array.length == 0
     puts "The line is currently empty."
-  else 
+  else
     messaged = "The line is currently:"
-    
-  array.each_with_index do |value, index| 
+
+  array.each_with_index do |value, index|
     message += " #{index.to_i+1}. #{value}"
-  end 
+  end
   puts "#{message}"
-  end 
+  end
 end 
