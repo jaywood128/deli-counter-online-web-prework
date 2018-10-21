507af4c5ad7ae0ca009783a8c7a0500a65efe54d
Johnathon Wood <jaywood128@gmail.com>
2018-10-21 19:12:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,5 +12,7 @@ def line(array)
 end
 def take_a_number(array, name)
   array.push(name)
+  position = array.index(name)
   puts"Welcome, #{array.index(name)}. You are number #{array.index(name) + 1} in line."
+  return name, position
 end
