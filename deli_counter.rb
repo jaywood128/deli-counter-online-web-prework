7ad72ed1902ca2da6d11c913d577a59d776ae1f4
Johnathon Wood <jaywood128@gmail.com>
2018-10-21 18:40:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,3 @@
-def line(array)
-  if array.length == 0
-    puts "The line is currently empty."
-  else
-    message = "The line is currently:"
-
-  array.each_with_index do |value, index|
-    message += " #{index.to_i+1}. #{value}"
-end
-end
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty"
