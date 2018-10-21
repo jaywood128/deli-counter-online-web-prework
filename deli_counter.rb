f9fd2019e6ddf45a488f8bcf8942b3ffcf0e256a
Johnathon Wood <jaywood128@gmail.com>
2018-10-21 19:03:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,5 +12,5 @@ def line(array)
 end
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts"Welcome, #{name}. You are number #{katz_deli.length}"
+  puts"Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
