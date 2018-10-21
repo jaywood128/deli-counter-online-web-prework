235d302fb420f28e665b57a3153408997be36655
Johnathon Wood <jaywood128@gmail.com>
2018-10-21 18:39:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,8 @@ def line(array)
 
   array.each_with_index do |value, index|
     message += " #{index.to_i+1}. #{value}"
-=======
+end
+end
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty"
@@ -15,7 +16,6 @@ def line(katz_deli)
     message = "The line is currently: "
   array.each_with_index do |value, index|
     message += "#{intex.to_i+1}. #{value}"
->>>>>>> 2b51938a77b2d630847591ddd9db468ffe3260b3
   end
   puts "#{message}"
   end
